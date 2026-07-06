3601c356ad622d4594f31354194ef87886434732
Gabriel <101530455+IPlayer0neI@users.noreply.github.com>
2026-07-06 16:26:05 -0300

    Modulirização da parte de criação de grafos

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
-Tarefa 1
+Tarefa 1</br>
 Para visualizar o grafo rode o arquivo tarefa1.py. Exemplo:
-python tarefa1.py
+> python tarefa1.py
 
 É possível usar outro parâmetros na forma:
-python tarefa1.py "lista de parâmetros"
+> python tarefa1.py "lista de parâmetros"
 
-parâmetros:
-save - Salva o grafo ao ínves de abrir uma janela para visualizá-lo
-both - Salva e abre a a janela
-airportName - Gera o grafo com as etiquetas do vertices como o nome dos respectivos aeroportos ao ínves do código IATA.
+Parâmetros:
+- save - Salva o grafo ao ínves de abrir uma janela para visualizá-lo
+- both - Salva e abre a a janela
+- airportName - Gera o grafo com as etiquetas do vertices como o nome dos respectivos aeroportos ao ínves do código IATA.
+
+Como usar a função gerarGrafoDeAeroportosERotas():
+importe ela:
+> from gerarGrafoDeAeroportosERotas import gerarGrafoDeAeroportosERotas 
+
+Depois chame ela é atribua a uma variável:
+
+> dadosDoGrafoDeAeroportosERotas = gerarGrafoDeAeroportosERotas()
+
+Ela iria retornar um dicionário com as seguintes propriedades:
+- GrafoDeAeroportosERotas: o próprio grafo feito com networkx
+- verticesDeAeroportos: um conjunto com vertices que representam cada um aeroporto. Eles são representados pelo código IATA
+- arestasDeRotas: um conjunto com arestas que representam as rotas entre aeroportos. Eles são uma tupla com (código IATA do aeroporto de origem, código IATA do aeroporto de desino)
---
+++ gerarGrafoDeAeroportosERotas.py
@@ -0,0 +1,84 @@
+import pandas as pd 
+#Biblioteca básica para formar o grafo
+import networkx as nx
+#Biblioteca para calcular a distância usando longitue e latitude.
+from haversine import haversine
+#Biblioteca padrão do python para fins de teste para gerar grafos diferentes de acordo com o argumento passado pela linha de comando no momento da chamada do script
+
+def gerarGrafoDeAeroportosERotas():
+    GrafoDeAeroportosERotas = nx.Graph()
+    #Dados na forma de um csv 
+    #Dados das companhias aéreas
+    companhiasAereas = pd.read_csv("airlines.csv")
+    #Dados das rotas aéreas
+    rotas = pd.read_csv("routes.csv")
+    #Dados dos aeroportos
+    aeroportos = pd.read_csv("airports.csv")
+    
+    #Pego as iinformações da silver airlines
+    silverAirlines = companhiasAereas[companhiasAereas[companhiasAereas.columns[1]] == "Silver Airways (3M)"]
+    #pego o código IATA dela
+    silverAirlinesIATA = silverAirlines.iloc[0, 3] 
+    
+    #Linhas aéreas da silver airlines
+    rotasDaSilverAirlines = rotas[rotas[rotas.columns[0]] == silverAirlinesIATA]
+    
+    #Vertices e arestas são conjuntos na forma do python, ou seja, um objeto do stipo Set
+    verticesDeAeroportos = set()
+    arestasDeRotas = set()
+    
+    #Passo por todas as rotas e pego os aeroportos de origem e e destino, tornando-os em vértices.
+    for indice, rota in rotasDaSilverAirlines.iterrows():
+        #Código IATA do aeroporto de origem
+        aeroportoDeOrigem = rota[rotasDaSilverAirlines.columns[2]]
+        #Código IATA do aeroporto de destino
+        aeroportoDeDestino = rota[rotasDaSilverAirlines.columns[4]]
+        
+        #Adiciona os aeroportos de origem e destino no conjunto de vertides de aeroportos, como é um conjunto duplicatas são ignoradas
+        verticesDeAeroportos.add(aeroportoDeOrigem)
+        verticesDeAeroportos.add(aeroportoDeDestino)
+    
+        #Adiciona os aeroportos de origem e destino no conjunto de vértices de rotas como uma aresta
+        arestasDeRotas.add((aeroportoDeOrigem, aeroportoDeDestino))
+    
+    GrafoDeAeroportosERotas.add_nodes_from(verticesDeAeroportos)
+    GrafoDeAeroportosERotas.add_edges_from(arestasDeRotas)
+    
+    #Passo por todas as arestas já catalogadas pego as informações de longitude a latitude entre os aeroportos e calculo a distânci entre eles e transformando em pesos
+    for aresta in arestasDeRotas:
+        aeroportoDeOrigem = aresta[0]
+        aeroportoDeDestino = aresta[1]
+        aeroportoDeOrigemDados = aeroportos[aeroportos[aeroportos.columns[4]] == aeroportoDeOrigem].iloc[0]
+        aeroportoDeDestinoDados = aeroportos[aeroportos[aeroportos.columns[4]] == aeroportoDeDestino].iloc[0]
+        
+        #Latitude do aeroporto de origem
+        latitudaDaOrigem = aeroportoDeOrigemDados[aeroportos.columns[6]]
+        #Longitude do aeroporto de origem
+        longitudeDaOrigem = aeroportoDeOrigemDados[aeroportos.columns[7]]
+    
+        #Latitude do aeroporto de origem
+        latitudaDoDestino = aeroportoDeDestinoDados[aeroportos.columns[6]]
+        #Longitude do aeroporto de origem
+        longitudeDoDestino = aeroportoDeDestinoDados[aeroportos.columns[7]]
+    
+        #Distância entre os dois aeroportos calculados com apoio da biblioteca haversine usando a longitude e latiude.
+        distância = haversine((latitudaDaOrigem, longitudeDaOrigem), (latitudaDoDestino, longitudeDoDestino))
+        #Adiciona a distância como peso no grafo
+        GrafoDeAeroportosERotas.edges[aeroportoDeOrigem, aeroportoDeDestino]["weight"] = float('{0:.2f}'.format(distância))
+    
+    #É um dicionário em que eu vou correlacionar um vértice que é o aeroporto e seu código IATA com o nome do aeroporto real na forma: {"Nome do aeroporto": }
+    etiquetas = {}
+    
+    for indice, aeroporto in aeroportos.iterrows():
+        nomeDoAeroporto = aeroporto[aeroportos.columns[1]]
+        codigoIATADoAeroporto = aeroporto[aeroportos.columns[4]]
+    
+        if codigoIATADoAeroporto in verticesDeAeroportos:
+            etiquetas[codigoIATADoAeroporto] = nomeDoAeroporto
+        
+    return {
+        "GrafoDeAeroportosERotas": GrafoDeAeroportosERotas,
+        "verticesDeAeroportos": verticesDeAeroportos,
+        "arestasDeRotas": arestasDeRotas,
+        "etiquetas": etiquetas
+    }
