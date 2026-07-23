e96a8075164c7ef4898300feab37875060430293
Gabriel Alves Sousa <alves.sousa@aluno.ufabc.edu.br>
2026-07-23 18:25:13 -0300

    Mudanças menores

--- tarefa1.py
+++ tarefa1.py
@@ -8,31 +8,29 @@ import sys
 from gerarGrafoDeAeroportosERotas import gerarGrafoDeAeroportosERotas 
 
 #Define o tamanho que o matplotlib irá usar para montar e mostrar o grafo
-plt.figure(figsize=(6, 6))
+plt.figure(figsize=(10, 10))
 plt.axis("off")
 
 dadosDoGrafoDeAeroportosERotas = gerarGrafoDeAeroportosERotas()
 GrafoDeAeroportosERotas = dadosDoGrafoDeAeroportosERotas["GrafoDeAeroportosERotas"]
 
 #Layout é uma variável que diz que como será desenhado o gráfico, existem várias formas de organizar um grafo, o networkx contém algumas delas já feitas, e uma delas é o spring_layout
-layout = nx.kamada_kawai_layout(GrafoDeAeroportosERotas, weight=40)
-
+layout = nx.kamada_kawai_layout(GrafoDeAeroportosERotas, weight=None)
 etiquetas = dadosDoGrafoDeAeroportosERotas["etiquetas"]
 
 #Etiquetas dos pesos do grafo
 edge_labels = nx.get_edge_attributes(GrafoDeAeroportosERotas, "weight")
-
 #Configurações de visualização
 if "airportName" in sys.argv:
     nx.draw_networkx_nodes(GrafoDeAeroportosERotas, pos=layout, node_size=400)
     nx.draw_networkx_edges(GrafoDeAeroportosERotas, pos=layout)
     nx.draw_networkx_labels(GrafoDeAeroportosERotas, pos=layout, labels=etiquetas, font_size=6.5)
-    nx.draw_networkx_edge_labels(GrafoDeAeroportosERotas, layout, edge_labels, font_size=5)
+    nx.draw_networkx_edge_labels(GrafoDeAeroportosERotas, layout, edge_labels, font_size=7)
 else:
     nx.draw_networkx_nodes(GrafoDeAeroportosERotas, pos=layout, node_size=400)
     nx.draw_networkx_edges(GrafoDeAeroportosERotas, pos=layout)
     nx.draw_networkx_labels(GrafoDeAeroportosERotas, pos=layout, font_size=6.5)
-    nx.draw_networkx_edge_labels(GrafoDeAeroportosERotas, layout, edge_labels, font_size=5)
+    nx.draw_networkx_edge_labels(GrafoDeAeroportosERotas, layout, edge_labels, font_size=7)
 
 #Escolhe se vai salvar como jpg, visualizar ou ambos
 plt.title("Grafo de aeroportos e rotas da Silver Airways (3M)")
